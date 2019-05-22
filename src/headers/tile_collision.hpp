3afccc44927fd60d57b837e418d36b783250a730
John Russell <50782633+JohnDevlopment@users.noreply.github.com>
2019-05-22 12:34:31 -0500

    Update tile_collision.hpp
    Changed the prototype for TileLocation(). New signature: accepts a rectangle and two integers.

--- src/headers/tile_collision.hpp
+++ src/headers/tile_collision.hpp
@@ -1,11 +1,11 @@
 #ifndef TILE_COLLISION_HPP
 #define TILE_COLLISION_HPP
 
-// PVector/Point class
-#include <custom/pointvector>
+// includes <pointvector>
+#include "line_point.h"
 
 // level loading library
-#include <custom/levelLoad.hpp>
+#include <levelLoad.hpp>
 
 // SDL headers
 #include "sdl_incs.h"
@@ -21,12 +21,12 @@ enum ENTRY_POINT {
 	ENTRY_PLAYER_UP,
 	ENTRY_PLAYER_LEFT,
 	ENTRY_PLAYER_RIGHT,
-//	ENTRY_PLAYER_INSIDE,
+	ENTRY_PLAYER_INSIDE,
 	ENTRY_SPRITE_DOWN,
 	ENTRY_SPRITE_UP,
 	ENTRY_SPRITE_LEFT,
 	ENTRY_SPRITE_RIGHT,
-//	ENTRY_SPRITE_INSIDE
+	ENTRY_SPRITE_INSIDE
 };
 
 // sprite being processed by the tile collision code
@@ -43,6 +43,6 @@ Point<int> GetXY(uint16_t uiTile, int iRowTiles);
 
 // get the location of a the origin of a tile located at (x,y)
 Point<int> TileLocation(int x, int y);
-SDL_Rect TileLocationInRect(int x, int y);
+void TileLocationInRect(SDL_Rect& rect, int x, int y);
 
 #endif /* TILE_COLLISION_HPP */
