11d891a056d6a4a1cbf8425a5b0cb0a25be9ddeb
JohnDevlopment <videoman042694@gmail.com>
2019-09-05 23:09:03 -0500

    Link Tcl commands
    Linked Tcl command <GetHash> to C function _private_GetHash. Linked Tcl command <level> to _private_LevelCommand.

--- src/headers/tcl.hpp
+++ src/headers/tcl.hpp
@@ -18,7 +18,12 @@ void TclCC_Quit();
 // allocate a character array with Tcl functions
 char* TclCC_AllocChar(size_t numBytes) UNUSED;
 
-// TODO add description
+// appends the value indicated by <val> to the dictionary <dict> under the key <key>
 int TclCC_AddToDict(Tcl_Interp* interp, Tcl_Obj** dict, const char* key, const char* val);
+
+/* Sets the string argument as the interpreter's string result. <len> is supposed to be
+the length of the string argument; but if it's negative, it will be set to the number of characters
+counted up to the NULL terminator. */
+void TclCC_SetErrorResult(Tcl_Interp* interp, const char* str, int len);
 
 #endif /* TCL_HPP */
--- src/tcl.cc
+++ src/tcl.cc
@@ -1,18 +1,15 @@
-// standard includes
-#include "headers/stdinc.h"
-
-#include "sound.hpp"
-#include "tcl.hpp"
-#include "sprite_router.hpp"
-#include "levelcode.hpp"
-#include "triggers.hpp"
-#include "log.hpp"
-#include "background_images.hpp"
 #include "camera.hpp"
-#include "levelinfo.hpp"
 #include "color.h"
+#include "game.hpp"
+#include "levelcode.hpp"
+#include "levelinfo.hpp"
+#include "log.hpp"
 #include "memory.hpp"
+#include "sound.hpp"
+#include "sprite_router.hpp"
+#include "stdinc.h"
 #include "string.h"
+#include "tcl.hpp"
 
 #define TCL_CC 1
 
@@ -20,7 +17,47 @@ using namespace std;
 
 Tcl_Interp* gInterp = nullptr;
 
+// private functions //
+static int _private_GetHash(ClientData cd, Tcl_Interp* interp, int objc, Tcl_Obj* const objv[]) {
+	const char* sStr;
+	int iStrLen;
+	long int lRetval;
+	
+	if (objc != 2) {
+	  Tcl_SetObjResult(gInterp, Tcl_NewStringObj("wrong # args: should be GetHash str", -1));
+	  return TCL_ERROR;
+	}
+	
+	// get string argument
+	sStr = Tcl_GetStringFromObj(objv[1], &iStrLen);
+	if (! sStr) {
+	  Tcl_Obj* result = Tcl_ObjPrintf("error returned from GetHash: failed to get string argument: %s",
+	                                 Tcl_GetStringResult(interp));
+	  Tcl_SetObjResult(interp, result);
+	  return TCL_ERROR;
+	}
+	
+	lRetval = static_cast<long int>(game::makeHash(sStr, iStrLen, 5000));
+	Tcl_SetObjResult(interp, Tcl_NewLongObj(lRetval));
+	
+return TCL_OK;
+}
+
+static int _private_LevelCommand(ClientData cd, Tcl_Interp* interp, int objc, Tcl_Obj* const objv[]) {
+
+
+
+return TCL_OK;
+}
+
 // public functions //
+void TclCC_SetErrorResult(Tcl_Interp* interp, const char* str, int len) {
+	if (len < 0)
+	  len = String_strlen(str);
+	
+	Tcl_SetObjResult(interp, Tcl_NewStringObj(str, len));
+}
+
 int TclCC_AddToDict(Tcl_Interp* interp, Tcl_Obj** dict, const char* key, const char* val) {
 	// key
 	Tcl_Obj* keyObj = Tcl_NewStringObj(key, -1);
@@ -48,6 +85,8 @@ return ptr_return;
 
 int TclCC_Init(PROGRAM* const program) {
 	using level::CurrentLevel;
+
+	char sName[31];
 	
 	Log_Cout("### Initializing Tcl ###\n");
 
@@ -71,6 +110,26 @@ int TclCC_Init(PROGRAM* const program) {
 	  return TCL_ERROR;
 	}
 	
+	// link GetHash to _private_GetHash
+	String_strcpy(sName, "GetHash");
+	if (! Tcl_CreateObjCommand(gInterp, sName, _private_GetHash, nullptr, nullptr)) {
+	  cerr << "Failed to link GetHash: " << Tcl_GetStringResult(gInterp) << '\n';
+	  return TCL_ERROR;
+	}
+	
+	// link "level" to _private_LevelCommand
+	String_strcpy(sName, "level");
+	if (! Tcl_CreateObjCommand(gInterp, sName, _private_LevelCommand, nullptr, nullptr)) {
+	  cerr << "Failed to link level command: " << Tcl_GetStringResult(gInterp) << '\n';
+	  return TCL_ERROR;
+	}
+	
+	// include script that defines globals
+	if (Tcl_Eval(gInterp, "set SIMPLELEVEL 1; source scripts/globals.tcl") == TCL_ERROR) {
+	  cerr << Tcl_GetStringResult(gInterp) << '\n';
+	  return TCL_ERROR;
+	}
+	
 	Log_Cout("Tcl variable CurrentLevel linked to C variable\nTcl initialized\n\n");
 	
 return TCL_OK;
@@ -78,5 +137,6 @@ return TCL_OK;
 
 void TclCC_Quit() {
 	Tcl_UnlinkVar(gInterp, "CurrentLevel");
+	Tcl_Free(level::CurrentLevel);
 	Tcl_DeleteInterp(gInterp);
 }
